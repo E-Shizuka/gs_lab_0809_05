93603fa177a4331d48bda5630639e0e519521ef1
E-Shizuka <snowman.r@gmail.com>
2023-08-09 17:29:24 +0900

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { client } from "../../libs/client";
+// SSGでデータ取得の場合はNext独自のgetStaticPropsを使って取得します😊
+export const getStaticProps = async () => {
+  const data = await client.get({
+    endpoint: "blog",
+  });
+
+  console.log(data.contents, "中身");
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  return (
+    <div>
+      <h1>ブログ</h1>
+      {blog &&
+        blog.map((item, index) => (
+          <div key={item.id}>
+            <Link href={`blog/${item.id}`}>
+              <h2>{item.title}</h2>
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+}
